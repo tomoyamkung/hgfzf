0057197eccbdbfc78cd2c39caf1344a4de873a7c
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-03-11 06:33:50 +0000

    refs #1 シェルスクリプトの dry-run に使用するライブラリを実装
    ----
    
    On branch feature/1
    Changes to be committed:
      - new file:   etc/lib/dry_run.sh

---
+++ etc/lib/dry_run.sh
@@ -0,0 +1,43 @@
+
+function usage_dryrun() {
+  cat <<EOF 1>&2
+Description:
+  シェルスクリプトを dry-run で実行する際に使用する変数と関数を定義したライブラリ。
+
+Usage:
+  dry-run を実行するための手順は以下の通り。
+
+  1. 本ライブラリを読み込む
+    - . ~/mercurial_tools/etc/lib/dry_run.sh
+  2. enable_dryrun() 関数を実行する
+  3. dry-run を実行したい場所で if を実行する
+
+  if の例は以下の通り。
+
+  ----
+  if [ ! -z ${dryrun} ]; then
+    echo "dry-run"
+  else
+    echo "execute"
+  fi
+  ----
+
+  変数 ${dryrun} には "echo" が格納される。
+  以下のようにコーディングすることで dry-run が有効になった際には ls は実行されず echo が実行されるようになる。
+
+  ----
+  ${dryrun} ls
+  ----
+EOF
+  exit 1
+}
+
+# dry-run をするかの判定に使用するフラグ
+# この変数に何か値が設定されている場合は dry-run とする
+# デフォルトはブランク、つまり、dry-run しない
+dryrun=
+
+# dry-run を有効にする関数
+function enable_dryrun() {
+  dryrun=echo
+}
